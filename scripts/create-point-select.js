163c5aa2beb9a3b17bde20a7839717f01c71d9d7
Philipi Magalhães <philipimagalhaes@gmail.com>
2020-06-05 11:47:06 -0300

    Pagina de coleta 100%

---
+++ scripts/create-point-select.js
@@ -0,0 +1,52 @@
+const apiEstadosUrl = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados';
+const ufSelect = document.querySelector('select[name=uf]');
+const ufInput = document.querySelector('[name = ufName]');
+const citySelect = document.querySelector('select[name=city]');
+const cityInput = document.querySelector('[name=cityName]');
+
+function setCityApiUrl(UFId) {
+    return `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${UFId}/municipios`
+}
+function getUFs() {
+    fetchFunction(apiEstadosUrl, ufSelect);
+}
+function fetchFunction(apiUrl, select) {
+    if(select === citySelect){
+        citySelect.disabled = true;
+        citySelect.innerHTML = "<option>Selecione a cidade </option>";        
+    }
+
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            data.sort((a, b) => a.nome.localeCompare(b.nome))
+                .map(dataItem => {
+                    select.innerHTML += `<option value="${dataItem.id}">${dataItem.nome}</option>`;
+                });
+        });
+}
+function getCities(event) {
+    const UFId = event.target.value;
+    const apiUrl = setCityApiUrl(UFId);
+    fetchFunction(apiUrl, citySelect);
+    citySelect.disabled = false;
+}
+function ufSelectChangeListener() {
+    ufSelect.addEventListener('change', getCities);
+    ufSelect.addEventListener('change', setUfValue);
+}
+function citySelectChangeListener() {
+    citySelect.addEventListener('change', setCityInput)
+}
+function setUfValue(event) {
+    const indexOfSelectedUF = event.target.selectedIndex;
+    ufInput.value = event.target.options[indexOfSelectedUF].text;
+}
+function setCityInput(event) {
+    const indexOfSelectedCity = event.target.selectedIndex;
+    cityInput.value = event.target.options[indexOfSelectedCity].text;
+}
+
+getUFs();
+ufSelectChangeListener();
+citySelectChangeListener();
--- scripts/create-point.js
+++ scripts/create-point.js
@@ -1,48 +1,32 @@
-const apiEstadosUrl = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados';
-const ufSelect = document.querySelector('select[name=uf]');
-const ufInput = document.querySelector('[name = ufName]');
-const citySelect = document.querySelector('select[name=city]');
-const cityInput = document.querySelector('[name=cityName]');
+const itemsToCollect = [...document.querySelectorAll('.items-grid li')];
+const itemsInput = document.querySelector('input[name=items]');
+let selectedItems = [];
 
-function setCityApiUrl(UFId) {
-    return `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${UFId}/municipios`
+function alreadySelected(selectedItems, itemId) {
+    return selectedItems.findIndex(selectedItem => {
+        return selectedItem === itemId
+    });
 }
-function getUFs() {
-    fetchFunction(apiEstadosUrl, ufSelect);
+function addOrRemoveItems(item) {
+    if (alreadySelected(selectedItems, item.dataset.id) >= 0) {
+        selectedItems = selectedItems.filter(selectedItem => {
+            return selectedItem != item.dataset.id;
+        })
+    }
+    else {
+        selectedItems.push(item.dataset.id);
+    }
 }
-function fetchFunction(apiUrl, select) {
-    fetch(apiUrl)
-        .then(response => response.json())
-        .then(data => {
-            data.sort((a, b) => a.nome.localeCompare(b.nome))
-                .map(dataItem => {
-                    select.innerHTML += `<option value="${dataItem.id}">${dataItem.nome}</option>`;
-                });
-        });
+function handleSelectionItem(event) {
+    const item = event.target;
+    item.classList.toggle('selected');
+    addOrRemoveItems(item);
+    itemsInput.value = selectedItems;
 }
-function getCities(event) {
-    const UFId = event.target.value;
-    const apiUrl = setCityApiUrl(UFId);
-    fetchFunction(apiUrl, citySelect);
-    citySelect.disabled = false;
-}
-function ufSelectChangeListener() {
-    ufSelect.addEventListener('change', getCities);
-    ufSelect.addEventListener('change', setUfValue);
-}
-function citySelectChangeListener() {
-    citySelect.addEventListener('change', setCityInput)
-}
-function setUfValue(event) {
-    const indexOfSelectedUF = event.target.selectedIndex;
-    ufInput.value = event.target.options[indexOfSelectedUF].text;
-}
-function setCityInput(event) {
-    const indexOfSelectedCity = event.target.selectedIndex;
-    cityInput.value = event.target.options[indexOfSelectedCity].text;
+function addOnClickItemsEventListeners(){
+    itemsToCollect.map(item => {
+        item.addEventListener('click', handleSelectionItem)
+    });
 }
 
-
-getUFs();
-ufSelectChangeListener();
-citySelectChangeListener();
+addOnClickItemsEventListeners();
